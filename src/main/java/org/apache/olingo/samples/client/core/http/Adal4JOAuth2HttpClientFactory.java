728879cd6739a195be3666f8d08564d75938b740
Manuel <wtfjoke@livenet.ch>
2018-12-07 12:16:40 +0100

    Some cleanup

--- src/main/java/org/apache/olingo/samples/client/core/http/Adal4JOAuth2HttpClientFactory.java
+++ src/main/java/org/apache/olingo/samples/client/core/http/Adal4JOAuth2HttpClientFactory.java
@@ -20,11 +20,8 @@ import java.util.concurrent.Future;
 
 public class Adal4JOAuth2HttpClientFactory extends AbstractOAuth2HttpClientFactory {
     private final String authority;
-    private final String clientId;
     private final String redirectURI;
     private final String resourceURI;
-    private final String clientSecret;
-    private final String authURL;
     private final ClientCredential clientCredentials;
     private String token;
     private AuthenticationContext context;
@@ -35,12 +32,9 @@ public class Adal4JOAuth2HttpClientFactory extends AbstractOAuth2HttpClientFacto
                                          final String redirectURI, final String resourceURI) {
         super(createGrantURL(authority, clientId, redirectURI, resourceURI), URI.create(authority + "/oauth2/token?resource=" + resourceURI));
         this.authority = authority;
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
         this.clientCredentials = new ClientCredential(clientId, clientSecret);
         this.redirectURI = redirectURI;
         this.resourceURI = resourceURI;
-        this.authURL = authority + "/oauth2/authorize?resource=" + resourceURI;
     }
 
     private static URI createGrantURL(String authority, String clientId, String redirectURI, String resourceURI) {
@@ -55,7 +49,6 @@ public class Adal4JOAuth2HttpClientFactory extends AbstractOAuth2HttpClientFacto
 
     @Override
     protected void init() throws OAuth2Exception {
-        //   String authority = "https://login.microsoftonline.com/contoso.onmicrosoft.com/";
         ExecutorService service = Executors.newFixedThreadPool(1);
         try {
             this.context = new AuthenticationContext(authority, true, service);
@@ -76,7 +69,7 @@ public class Adal4JOAuth2HttpClientFactory extends AbstractOAuth2HttpClientFacto
         final Future<AuthenticationResult> yay = this.context.acquireTokenByAuthorizationCode(authorizationCode, URI.create(redirectURI), clientCredentials, resourceURI, new AuthenticationCallback() {
             @Override
             public void onSuccess(Object o) {
-                System.out.println("Yay,token aquired!");
+                System.out.println("Yay, acquired auth-token!");
             }
 
             @Override
@@ -84,14 +77,11 @@ public class Adal4JOAuth2HttpClientFactory extends AbstractOAuth2HttpClientFacto
                 throwable.printStackTrace();
             }
         });
-        final AuthenticationResult authenticationResult;
         try {
-            authenticationResult = yay.get();
+            AuthenticationResult authenticationResult = yay.get();
             this.token = authenticationResult.getAccessToken();
             this.refreshToken = authenticationResult.getRefreshToken();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
     }
@@ -106,10 +96,14 @@ public class Adal4JOAuth2HttpClientFactory extends AbstractOAuth2HttpClientFacto
 
     @Override
     protected void refreshToken(DefaultHttpClient defaultHttpClient) throws OAuth2Exception {
-        this.context.acquireTokenByRefreshToken(refreshToken, clientId, resourceURI, new AuthenticationCallback() {
+        this.context.acquireTokenByRefreshToken(refreshToken, clientCredentials.getClientId(), resourceURI, new AuthenticationCallback() {
             @Override
             public void onSuccess(Object o) {
-                System.out.println("Sucessfully issued token by refreshtoken");
+                if (token != null) {
+                    System.out.println("Sucessfully issued token by refreshtoken");
+                } else {
+                    throw new OAuth2Exception("No OAuth2 refresh token");
+                }
             }
 
             @Override
